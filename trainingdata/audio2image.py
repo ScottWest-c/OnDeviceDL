f70f5afc7aec0a613db3a7130f3699ee05d6ece0
asanjay3 <asanjay3@wpi.edu>
2025-02-02 20:37:43 -0500

    STFTs of Valentini Dataset

--- trainingdata/audio2image.py
+++ trainingdata/audio2image.py
@@ -1,9 +1,9 @@
-#import kagglehub
+import kagglehub
 from glob import glob
 import librosa
 import librosa.display
 import numpy as np
-import matplotlib as matplotlib
+import matplotlib.pyplot as plt
 import pandas as pd
 from pathlib import Path
 import os
@@ -16,58 +16,51 @@ from PIL import Image
 #test_audio_file = "/Users/achintya.san/Desktop/CS556FinalProject/trainingdata/archive-2/Actor_16/03-01-05-01-02-01-16.wav"
 # test_audio_file = "archive-2/Actor_01/03-01-01-01-01-01-01.wav"
 
-input_dir = Path.cwd() / "archive-2"
-files = list(input_dir.rglob("*.wav*"))
-# spectograms = []
-i = 0
-for file in files:
-    audio_file = file
 
-    audio, sample_rate = librosa.load(audio_file)
 
-    FRAME_SIZE = 2048
-    HOP_SIZE = 512
-    S_audio = librosa.stft(audio, n_fft=FRAME_SIZE, hop_length=HOP_SIZE)
-    #print(S_audio.shape)
-    Y_audio = np.abs(S_audio) ** 2
-    #print(Y_audio.shape)
+train_label_1 = Path.cwd() / "archive-3/clean_trainset_28spk_wav"
+train_label_2 = Path.cwd() / "archive-3/clean_trainset_56spk_wav"
 
-    def plot_spectogram(Y, sr, hop_length, y_axis="linear"):
-        matplotlib.pyplot.figure(figsize=(25,10))
-        librosa.display.specshow(Y,
-                                sr=sr,
-                                hop_length=hop_length,
-                                x_axis="time",
-                                y_axis=y_axis)
-        matplotlib.pyplot.colorbar(format="%+2.f")
-        #matplotlib.pyplot.close()
+train_data_1= Path.cwd() / "archive-3/noisy_trainset_28spk_wav"
+train_data_2 = Path.cwd() / "archive-3/noisy_trainset_56spk_wav"
 
-    #plot_spectogram(Y_audio, sample_rate, HOP_SIZE)
+#files = list(input_dir.rglob("*.wav*"))
+#files = ["archive-3/noisy_trainset_56spk_wav/p226_001.wav"]
+#files[0] = "archive-3/noisy_trainset_56spk_wav/p226_001.wav"
 
-    Y_log_audio = librosa.power_to_db(Y_audio)
-    #print(Y_log_audio.shape)
-    #plot_spectogram(Y_log_audio, sample_rate, HOP_SIZE, y_axis="log")
+i = 1
+#for file in files:
+#audio_file = file
+audio_file = "archive-3/noisy_trainset_28spk_wav/p226_001.wav"    #.02*sample_rate 
 
-    if not isinstance(Y_log_audio, Image.Image):
-        Y_log_audio = Image.fromarray(Y_log_audio)
-        if Y_log_audio.mode != 'RGB':
-            Y_log_audio = Y_log_audio.convert('RGB')
+audio, sample_rate = librosa.load(audio_file)
+#print(sample_rate)
 
-    filename = os.path.join("spectogram_images", f"spectogram_{i}.jpg")
-    Y_log_audio.save(filename)
-    i = i + 1
+FRAME_SIZE = 2048
+HOP_SIZE = 512
+S_audio = librosa.stft(audio, n_fft=FRAME_SIZE, hop_length=HOP_SIZE)
+Y_audio = np.abs(S_audio) ** 2
 
-    # spectograms.append(Y_log_audio)
 
-# for i, spectogram in enumerate(spectograms):
-#     if not isinstance(spectogram, Image.Image):
-#         spectogram = Image.fromarray(spectogram)
-#         if spectogram.mode != 'RGB':
-#             spectogram = spectogram.convert('RGB')
+Y_log_audio = librosa.power_to_db(Y_audio)
 
-#     filename = os.path.join("spectogram_images", f"spectogram_{i}.jpg")
+plt.figure(figsize=(10,6))
+librosa.display.specshow(librosa.amplitude_to_db(Y_log_audio, ref=np.max), sr=sample_rate, hop_length=HOP_SIZE)
+# plt.colorbar(format='%+2.0f dB')
+# plt.title("Spectogram")
+plt.axis("off")
+plt.gca().set_xticks([])  # Remove x ticks
+plt.gca().set_yticks([])  # Remove y ticks
+plt.subplots_adjust(left=0, right=1, top=1, bottom=0)  # Remove extra padding
+plt.savefig(f"spectogram_images/spectogram_{i}.png")
+plt.close()  
+i = i + 1
+#print(Y_log_audio.shape)
+#plot_spectogram(Y_log_audio, sample_rate, HOP_SIZE, y_axis="log")
 
-#     spectogram.save(filename)
+
+
+
 
  
 
